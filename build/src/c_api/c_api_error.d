a12f95bc8969435d44942329425835e5435652d4
xcgoner <xcgoner1108@gmail.com>
2016-10-26 14:59:59 -0500

    new repo

---
+++ build/src/c_api/c_api_error.d
@@ -0,0 +1,6 @@
+build/src/c_api/c_api_error.o: src/c_api/c_api_error.cc \
+ src/c_api/./c_api_error.h \
+ /home/cx2/ClionProjects/ps-mxnet/dmlc-core/include/dmlc/base.h \
+ /home/cx2/ClionProjects/ps-mxnet/dmlc-core/include/dmlc/logging.h \
+ /home/cx2/ClionProjects/ps-mxnet/dmlc-core/include/dmlc/./base.h \
+ include/mxnet/c_api.h src/c_api/../common/thread_local.h
---
+++ build/src/operator/mkl/mkl_cppwrapper.d
@@ -0,0 +1,2 @@
+build/src/operator/mkl/mkl_cppwrapper.o: \
+ src/operator/mkl/mkl_cppwrapper.cc src/operator/mkl/mkl_cppwrapper.h
